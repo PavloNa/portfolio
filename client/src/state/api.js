3fd03562fdb1aec3fab3cf8923e54bbe92a7e4f8
pavlo <pavlo.escola@gmail.com>
2023-03-04 22:27:07 +0000

    Added header / Middleware api fix / Added choropleth

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,9 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL}),
     reducerPath: "adminApi",
+    tagTypes:[
+        "Geography"
+    ],
     endpoints: (build) => ({
         getLanguage: build.query({
-            query: `personal/languages`
+            query: () => `personal/languages`,
+            providesTags: ["Geography"],
         })
     })
 })
--- server/controllers/personal.js
+++ server/controllers/personal.js
@@ -2,7 +2,8 @@ import Language from "../models/Language.js";
 
 export const getLanguage = async (req, res) => {
     try {
-
+        const languageSkills = await Language.find()
+        res.status(200).json(languageSkills)
     } catch (error) {
         res.status(404).json({message: error.message})
     }
